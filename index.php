613fc1f55959eef59c6cf63d318f731bc230fe5b
luca-gif <luca.89m@gmail.com>
2022-06-08 14:26:36 +0200

    Prima repo

---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- Riscrivere questa pagina del sito google, ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+Per la formattazione del testo della FAQ inserite il markup HTML direttamente nel “database”
+ -->
+
+
+
+<?php
+
+
+
+?>
